08ba6231a5ef5d6ece8656dc369eefcac07857d7
bsu <bsu0541@naver.com>
2020-12-20 01:26:01 +0900

    score.c 점수 정렬 기능 추가

--- score.c
+++ score.c
@@ -11,8 +11,12 @@
 #include <fcntl.h>
 #include "gets_header.h"
 
-void sBoard() {
-	FILE *f = fopen("score.txt", "r");
+struct ranking {
+	char name[10];
+	int score;
+};
+
+void scoreBoard() {
 	char name[10], buf[30], input;
 	int score, i = 1;
 
@@ -21,14 +25,8 @@ void sBoard() {
 	mvaddstr(3,30,"+---SCOREBOARD---+");
 	mvaddstr(5,28,"+RANK----NAME----SCORE+");
     
-    if(f == NULL) {
-        mvaddstr(10, 30, "NO SCORE SUBMITTED");
-    } else {
-        while (fscanf(f, "%s %d", name, &score) != EOF) {
-            sprintf(buf,"%2d:%8s    %5d",i++, name, score);
-            mvaddstr(5+i,30,buf);
-        }
-    }
+	sort_score();
+
 	mvaddstr(MAP_HEIGHT-1,2,"Press 'q' to return to main...");
 	refresh();
 	while (1) {
@@ -36,6 +34,38 @@ void sBoard() {
 		if(input == 'q') {
 			break;
 		}
+	}
+}
+
+void sort_score() {
+	struct ranking* r;
+    	struct ranking tmp;
+	int i = 0, buf[30];
+	FILE *f = fopen("score.txt", "r");
+
+	r = (struct ranking*)calloc(30, sizeof(struct ranking)); //할당 크기
+    	while (fscanf(f, "%s %d", r[i].name, &r[i].score) != EOF) {
+        	i++;
+    	}
+    	for (int a = 0; a < 10; a++) { //for size
+        	for (int b = 0; b < 10-a-1; b++) {
+            		if (r[b].score < r[b+1].score) {
+                		tmp = r[b];
+                		r[b] = r[b + 1];
+                		r[b + 1] = tmp;
+            		}
+        	}
+    	}
+	if (f == NULL) {
+		mvaddstr(10,30,"NO SCORE SUBMITTED");
+	}
+	else {
+		int n = 0;
+		do {
+			sprintf(buf,"%2d:%8s    %5d",n+1, r[n].name, r[n].score);
+			n++;
+			mvaddstr(5+n,30,buf);
+		}while(n<10);
 	}
 	fclose(f);
 }
