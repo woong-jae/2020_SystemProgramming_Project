3a66a00bae24cd0353767b7098c7820a19f087e5
정재웅 <woong_jae@MacBook-Pro.local>
2020-12-20 00:39:09 +0900

    game.c updated

--- game.c
+++ game.c
@@ -10,198 +10,136 @@
 #define MAP_HEIGHT 20
 #define DATA_SIZE 3
 
-char* data[] = { "aaaaaa", "asdf", "abcde" };
 
-typedef struct Word
+char* data[] = { "aaaa", "asdf", "abcde" };
+
+typedef struct Word {
+    char *str;
+	int row, col;
+    struct Word *next;
+} word;
+word *wordlist = NULL;
+
+int hp = 3;
+
+void draw_border(void);
+void *word_flow(void *none);
+void append_wordlist(void);
+word* make_word(void);
+int delete_word(char *input);
+void reset_wordlist(void);
+void move_word(word* w);
+int main(int argc, const char * argv[])
 {
-	char str[100];
-	int row;
-	int col;
-}Word;
-
-Word list[100];
-int score = 0;
-int life = 3;
-pthread_t pthread;
-pthread_mutex_t lock;
-
-char* MakeStr()
-{
-	srand((int)time(NULL));
-	int index = rand() % DATA_SIZE;
-	return data[index];
+    pthread_t t;
+    srand((int)time(NULL));
+    
+    initscr();
+    clear();
+    draw_border();
+    move(MAP_HEIGHT - 1 , 2);
+    refresh();
+    
+    pthread_create(&t, NULL, word_flow, NULL);
+    while(hp > 0){
+        char input = getch();
+    }
+    pthread_join(t, NULL);
+    
+    endwin();
+    return 0;
 }
 
-void Flow(int i)
-{
-	int t;
-	for (t = 0; t <= i; t++)
-		list[t].col += 4;
+void move_word(word* w) {
+    int str_len = (int)sizeof(w->str);
+    char *blank = (char*)malloc(str_len);
+    memset(blank, ' ', str_len);
+    blank[sizeof(str_len) - 1] = 0;
+    mvaddstr(w->row, w->col, blank);
+    w->col = w->col + 1;
+    mvaddstr(w->row, w->col, w->str);
+    refresh();
+    free(blank);
 }
 
-void Draw(int row, int col, char *str)
-{
-	move(row, col);
-	addstr(str);
-	move(81,30);
-	refresh();
+void reset_wordlist(void) {
+    word *cur = wordlist;
+    word *next = cur->next;
+    while(cur != NULL) {
+        free(cur);
+        cur = next;
+        next = cur->next;
+    }
 }
 
-void CreateList(char* str, int i)
-{
-	srand((int)time(NULL));
-	strcpy(list[i].str, str);
-	list[i].row = rand() % (MAP_HEIGHT-4) + 1;
-	list[i].col = 2;
-	usleep(2000000);
-	Flow(i);
-}
-
-void Blank_OutputWord()
-{
-	for(int i=1;i<MAP_HEIGHT-2;i++)
-	{
-		move(i,1);
-		addstr("                                                                             ");
-	}
-}
-
-void draw_border()
-{
-	for(int i = 0 ; i < MAP_WIDTH; i++) {
-		mvaddch(0, i, '-');
-   	}
-   	for(int i = 0 ; i < MAP_WIDTH; i++) {
-       		mvaddch(MAP_HEIGHT, i, '-');
-    	}
-    	for(int i = 1 ; i < MAP_HEIGHT; i++) {
-        	mvaddch(i, 0, '|');
-    	}
-    	for(int i = 1 ; i < MAP_HEIGHT; i++) {
-        	mvaddch(i, MAP_WIDTH - 1, '|');
-    	}
-	mvaddstr(MAP_HEIGHT-2,1,"---------------------------------------------");
-}
-
-void* Game_Play(void *arg)
-{
-        int i = 0, t;
-        while(1)
-        {
-                CreateList(MakeStr(), i);
-                Blank_OutputWord();
-                for(t = 0; t<=i; t++)
-                {
-                        if(list[t].col<MAP_WIDTH-2)
-			{
-				Draw(list[t].row,list[t].col,list[t].str);
-                        
-			}
-			else
-                        {
-                                life--;
-                                if(life==0)
-                                {
-                                        //GameOver();
-
-                                }
-
-                        }
-			
-                }
-		//move(81,30);
-		
-                i++;
-                if(i>=100)
-                        i = 0;
+int delete_word(char *input) {//발견 실패 시 '0' 반환, 성공 시 '1' 반환
+    word *cur = wordlist;
+    word *prev = NULL;
+    while(cur != NULL) {
+        if(strcmp(cur->str, input) == 0) {//처음
+            if(cur == wordlist) {
+                wordlist = cur->next;
+            } else if (cur->next == NULL) {//끝
+                prev->next = NULL;
+            } else {
+                prev->next = cur->next;
+            }
+            free(cur);
+            return 1;
         }
+        prev = cur;
+        cur = cur->next;
+    }
+    return 0;
 }
 
-void* User_Input();
-
-int main()
-{
-
-	initscr();
-	draw_border();
-	memset(list,0,sizeof(list));
-	
-
-	pthread_t pthread2;
-
-	pthread_create(&pthread, NULL, Game_Play, NULL);
-	pthread_create(&pthread2, NULL, User_Input, NULL);
-	/*
-	while(life > 0)
-	{
-		for(num = 0; num < 50;)
-		{
-			if(life == 0)
-				pthread_exit(NULL);
-			ch = getch();
-			if(ch == '\n')
-			{
-				input[num] = '\0';
-				//FindWords();
-				for(int i=0; i<50; i++)
-					input[i] = '\0';
-				move(81, 30);
-				addstr("                                    ");
-				move(81, 30);
-				break;
-			}
-			else
-			{
-				input[num++] = ch;
-				move(81, 30);
-				addstr(input);
-			}
-			refresh();
-		}
-	}
-	*/
-	pthread_join(pthread, NULL);
-	pthread_join(pthread2, NULL);
-	Blank_OutputWord();
-	refresh();
-	clear();
-	
-	return 0;
+word* make_word(void) {
+    word *new = (word *)malloc(sizeof(word));
+    new->str = data[rand() % DATA_SIZE];
+    new->row = rand() % (MAP_HEIGHT-4) + 1;
+    new->col = 1;
+    new->next = NULL;
+    return new;
 }
 
-void* User_Input()
-{
-	int i = 0, t;
-        char input[50];
-        int num, ch;
-
-	 
-	while(life > 0)
-        {
-                for(num = 0; num < 50;)
-                {
-                        if(life == 0)
-                                pthread_exit(NULL);
-                        ch = getch();
-                        if(ch == '\n')
-                        {
-                                input[num] = '\0';
-                                //FindWords();
-                                for(int i=0; i<50; i++)
-                                        input[i] = '\0';
-                                move(81, 30);
-                                addstr("                                    ");
-                                move(81, 30);
-                                break;
-                        }
-                        else
-                        {
-                                input[num++] = ch;
-                                move(81, 30);
-                                addstr(input);
-                        }
-                        refresh();
-                }
+void append_wordlist(void) {
+    sleep(1);
+    if (!wordlist) {
+        wordlist = make_word();
+    } else {
+        word* cur = wordlist;
+        while(cur->next != NULL) {
+            cur = cur->next;
         }
+        cur->next = make_word();
+    }
 }
 
+void *word_flow(void *none) {//화면에 글을 출력
+    while(1) {
+        append_wordlist();
+        word *cur = wordlist;
+        while(cur) {
+            move_word(cur);
+            cur = cur->next;
+        }
+        move(MAP_HEIGHT - 1 , 2);
+        refresh();
+        usleep(500000);
+    }
+}
+
+void draw_border(void) {
+    for(int i = 0 ; i < MAP_WIDTH; i++) {
+        mvaddch(0, i, '-');
+    }
+    for(int i = 0 ; i < MAP_WIDTH; i++) {
+        mvaddch(MAP_HEIGHT, i, '-');
+    }
+    for(int i = 1 ; i < MAP_HEIGHT; i++) {
+        mvaddch(i, 0, '|');
+    }
+    for(int i = 1 ; i < MAP_HEIGHT; i++) {
+        mvaddch(i, MAP_WIDTH - 1, '|');
+    }
+}
